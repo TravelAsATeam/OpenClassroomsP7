22b12c6b8cfdc6951b4a9c4af791f8b04d31b791
Timothe Vieillescazes <timothe.vieillescazes@live.fr>
2021-12-03 20:06:25 +0100

    Maj dashboard, prise en compte du csv non normalise

--- dashboard.py
+++ dashboard.py
@@ -13,6 +13,7 @@ import os
 # Téléchargement des données
 default_dir = os.getcwd()
 data = pd.read_csv(os.path.join(default_dir,'data_sampled.csv'))
+data_chart = pd.read_csv(os.path.join(default_dir,'data_chart_sampled.csv'))
 
 # Choix du mode de fonctionnement
 mode_predict = False
@@ -21,16 +22,17 @@ if st.button("Faire une prediction"):
 
 # Mode affichage de graphique
 if mode_predict == False :
-    features = st.multiselect("Choisissez deux variables", list(data.columns))
+    features = st.multiselect("Choisissez deux variables", list(data_chart.columns))
     if len(features) != 2 :
         st.error("Sélectionnez deux variables")
     else :
         st.write("## Graphique bi-varié")
         # Graphique
-        chart = sns.jointplot(x=data[features[0]], y=data[features[1]], height=10)
+        chart = sns.jointplot(x=data_chart[features[0]], y=data_chart[features[1]], height=10)
         # Regression linéaire
-        sns.regplot(x=data[features[0]], y=data[features[1]], scatter=False, ax=chart.ax_joint)
+        sns.regplot(x=data_chart[features[0]], y=data_chart[features[1]], scatter=False, ax=chart.ax_joint)
         st.pyplot(chart)
+    st.button("Recommencer")
         
 # Mode prédiction
 if mode_predict == True :
