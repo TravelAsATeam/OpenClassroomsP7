9dba375176c8971346c8098042ffa33f38d7e8ff
Timothe Vieillescazes <timothe.vieillescazes@live.fr>
2021-12-03 17:44:54 +0100

    Test du dashboard

---
+++ dashboard.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+import streamlit as st
+import pandas as pd
+import seaborn as sns
+import os
+
+from urllib.error import URLError
+
+# Téléchargement des données
+default_dir = os.getcwd()
+data = pd.read_csv(os.path.join(default_dir,'data_sampled.csv'))
+
+# Choix du mode de fonctionnement
+mode_predict = False
+mode_chart = False
+if st.button("Faire une prediction"):
+    mode_predict = True
+if st.button("Afficher un graphique"):
+    mode_chart = True
+
+# Mode affichage de graphique
+if mode_chart == True :
+    features = st.multiselect("Choisissez deux variables", list(data.columns))
+    if len(features) != 2 :
+        st.error("Sélectionnez deux variables")
+    else :
+        st.write("## Graphique bi-varié")
+        # Graphique
+        chart = sns.jointplot(x=data[features[0]], y=data[features[1]], height=10)
+        # Regression linéaire
+        sns.regplot(x=data[features[0]], y=data[features[1]], scatter=False, ax=graph.ax_joint)
+        
+# Mode prédiction
+if mode_predict == True :
+    st.error("Non implementé")
+ 
+
+
+# In[5]:
+
+
+1 !=2
+
+
+# In[ ]:
+
+
+
+
